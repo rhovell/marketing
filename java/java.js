a5d0740ff1e052ebacaa8e5527b9c2aeebeaf5f6
Rachel Hovell <r.hovell@live.com>
2019-02-05 18:19:09 +0000

    State update

--- java/java.js
+++ java/java.js
@@ -1,29 +1,24 @@
 // SLIDESHOW
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+var slideIndex = 0;
+carousel();
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(carousel, 4000); // Change image every 4 seconds
 }
 // END OF SLIDESHOW
 
@@ -65,3 +60,51 @@ function hideSection(event){
   // adds event listener to hide section
   parent.addEventListener('click', showSection);
 }
+
+// END OF SECTION SELECTION
+manageMenus();
+let w = window.innerWidth;
+function manageMenus(){
+
+if(w < 480){
+  const menu = document.querySelector('#navi');
+  if(menu.classList.contains === 'open'){
+    menu.classList.remove('open');
+  }
+}
+if(w > 480){
+  if(menu.classList.contains !== 'open'){
+    menu.classList.add('open');
+  }
+}
+}
+
+window.addEventListener("resize", manageMenus);
+
+  // show menu on burger icon click
+    function showMenu(){
+      const menuButton = document.querySelector("#burgerMenu");
+      const navi = document.querySelector("#navi");
+      const menuLink = document.querySelector(".nav-list");
+      console.log("show menu")
+      // add open to navi list
+      navi.classList.add("open");
+      // remove event listener to show menu
+      menuButton.removeEventListener("click", showMenu);
+      // add event listener to hide menu
+      menuButton.addEventListener("click", hideMenu);
+      // add hidemenu event listener to menu items
+      menuLink.addEventListener("click", hideMenu);
+    }
+  // hide menu on icon click
+    function hideMenu(){
+      const menuButton = document.querySelector("#burgerMenu");
+      const navi = document.querySelector("#navi");
+      const menuLink = document.querySelector(".nav-list");
+      // remove open from navi list
+      navi.classList.remove("open");
+      // add event listener to menu button to show menu
+      menuButton.addEventListener("click", showMenu);
+      // add event listener to menu button to hide menu
+      menuButton.removeEventListener("click", hideMenu);
+    }
---
+++ java/page.js
@@ -0,0 +1,17 @@
+// SLIDESHOW
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 4000).delay(10000); // Change image every 4 seconds
+}
+// END OF SLIDESHOW
